f609a6c66a9809ffcb17f1c3658eedfc23b693f4
Audrey <ale314@uw.edu>
2023-05-18 18:53:04 -0700

    Lesson 7

--- mysite/urls.py
+++ mysite/urls.py
@@ -2,6 +2,7 @@ from django.contrib import admin
 from django.urls import path, include  # <-- Make sure you have both of these imports.
 
 urlpatterns = [
-    #path('polling/', include('polling.urls')),
+    path('polling/', include('polling.urls')),
+    path('', include('blogging.urls')),
     path('admin/', admin.site.urls),
 ]
