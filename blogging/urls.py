5abb17bf5d32ed5ec6f92a35bb4b42c193bdaa20
Audrey <ale314@uw.edu>
2023-06-03 16:16:06 -0700

    assignment 8 completed

--- blogging/urls.py
+++ blogging/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 from blogging.admin import admin_site
-from blogging.views import list_view, detail_view
+from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("blog_admin/", admin_site.urls),
-    path('', list_view, name="blog_index"),
-    path('posts/<int:post_id>/', detail_view, name="blog_detail"),
+    path('', BlogListView().as_view(), name="blog_index"),
+    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -1,33 +1,31 @@
-from django.http import HttpResponse, HttpResponseRedirect, Http404
-from django.template import loader
 from blogging.models import Post
 from django.shortcuts import render
-
-def stub_view(request, *args, **kwargs):
-    body = "Stub View\n\n"
-    if args:
-        body += "Args:\n"
-        body += "\n".join(["\t%s" % a for a in args])
-    if kwargs:
-        body += "Kwargs:\n"
-        body += "\n".join(["\t%s: %s" % i for i in kwargs.items()])
-    return HttpResponse(body, content_type="text/plain")
+from django.views.generic.detail import DetailView
 
 
-def list_view(request):
-    published = Post.objects.exclude(published_date__exact=None)
-    posts = published.order_by('-published_date')
-    template = loader.get_template('blogging/list.html')
-    context = {'posts': posts}
-    # body = template.render(context)
-    # return HttpResponse(body, content_type="text/html")
-    return render(request, 'blogging/list.html', context)
+class ListView():
+    def as_view(self):
+        return self.get
 
-def detail_view(request, post_id):
-    published = Post.objects.exclude(published_date__exact=None)
-    try:
-        post = published.get(pk=post_id)
-    except Post.DoesNotExist:
-        raise Http404
-    context = {'post': post}
-    return render(request, 'blogging/detail.html', context)
+    def get(self, request):
+        model_list_name = self.model.__name__.lower() + '_list'
+        # the exclude method will not display another post I created called 'test exclude'
+        # because it doesn't have a published date.
+        queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
+        context = {model_list_name: queryset}
+        return render(request, self.template_name, context)
+
+
+class BlogListView(ListView):
+    model = Post
+    template_name = 'blogging/list.html'
+
+
+class BlogDetailView(DetailView):
+    model = Post
+    template_name = 'blogging/detail.html'
+
+    def post(self, request, *args, **kwargs):
+        blog_post = self.get_object()
+        context = {'object': blog_post}
+        return render(request, 'blogging/detail.html', context)
