e0da39c98bdc56b822fb7e4ae3d6d016fdf38e3d
Audrey <ale314@uw.edu>
2023-06-09 22:37:37 -0700

    Applied black.

--- blogging/urls.py
+++ blogging/urls.py
@@ -4,6 +4,6 @@ from blogging.views import BlogListView, BlogDetailView
 
 urlpatterns = [
     path("blog_admin/", admin_site.urls),
-    path('', BlogListView().as_view(), name="blog_index"),
-    path('posts/<int:pk>/', BlogDetailView.as_view(), name="blog_detail"),
+    path("", BlogListView().as_view(), name="blog_index"),
+    path("posts/<int:pk>/", BlogDetailView.as_view(), name="blog_detail"),
 ]
--- blogging/views.py
+++ blogging/views.py
@@ -3,29 +3,31 @@ from django.shortcuts import render
 from django.views.generic.detail import DetailView
 
 
-class ListView():
+class ListView:
     def as_view(self):
         return self.get
 
     def get(self, request):
-        model_list_name = self.model.__name__.lower() + '_list'
+        model_list_name = self.model.__name__.lower() + "_list"
         # the exclude method will not display another post I created called 'test exclude'
         # because it doesn't have a published date.
-        queryset = Post.objects.order_by('-published_date').exclude(published_date__exact=None)
+        queryset = Post.objects.order_by("-published_date").exclude(
+            published_date__exact=None
+        )
         context = {model_list_name: queryset}
         return render(request, self.template_name, context)
 
 
 class BlogListView(ListView):
     model = Post
-    template_name = 'blogging/list.html'
+    template_name = "blogging/list.html"
 
 
 class BlogDetailView(DetailView):
     model = Post
-    template_name = 'blogging/detail.html'
+    template_name = "blogging/detail.html"
 
     def post(self, request, *args, **kwargs):
         blog_post = self.get_object()
-        context = {'object': blog_post}
-        return render(request, 'blogging/detail.html', context)
+        context = {"object": blog_post}
+        return render(request, "blogging/detail.html", context)
