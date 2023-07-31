b04b5b72207abfc6070459d56eab558342dfdbea
Audrey <ale314@uw.edu>
2023-07-31 15:53:40 -0700

    github auth works

--- mysite/urls.py
+++ mysite/urls.py
@@ -1,11 +1,13 @@
 from django.contrib import admin
 # from blogging.admin import admin_site
+# from .views import Home
 from django.urls import path, include
 from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
-    #path('polling/', include('polling.urls')),
+    path('polling/', include('polling.urls')),
     path('', include('blogging.urls')),
+    # path('', Home.as_view(), name='home'),
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('login/', LoginView.as_view(template_name='login.html'), name="login"),
