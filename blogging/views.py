f0bc42b2e76a89820cbf1fce999fe739a89413e4
Audrey <ale314@uw.edu>
2023-07-31 21:44:27 -0700

    add form

--- blogging/views.py
+++ blogging/views.py
@@ -1,6 +1,8 @@
 from blogging.models import Post
-from django.shortcuts import render
+from blogging.forms import PostForm
 from django.views.generic.detail import DetailView
+from django.shortcuts import render, redirect
+from django.contrib import messages
 
 
 class ListView():
@@ -29,3 +31,23 @@ class BlogDetailView(DetailView):
         blog_post = self.get_object()
         context = {'object': blog_post}
         return render(request, 'blogging/detail.html', context)
+
+
+# class AddModel(PostForm):
+#     model = Post
+#     template_name = 'blogging/add.html'
+
+def homepage(request):
+    if request.method == "POST":
+        post_form = PostForm(request.POST)
+        if post_form.is_valid():
+            post_form.save()
+            messages.success(request, 'Your post was successfully added!')
+        else:
+            messages.error(request, 'Error saving form')
+
+        return redirect("main:homepage")
+    post_form = PostForm()
+    posts = Post.objects.all()
+    return render(request=request, template_name="blogging/add.html",
+                  context={'post_form': post_form, 'posts': posts})
